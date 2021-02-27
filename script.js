6a0742b050a6390d7867e9ba8a24587eb551564c
Beta-23 <alfred.dominguez@gmail.com>
2021-02-26 21:11:21 -0800

    Update test data

--- script.js
+++ script.js
@@ -78,8 +78,8 @@ function fetchBookmarks() {
         // Create bookmarks array in localStorage
         bookmarks = [
             {
-                name: 'Jacinto Design',
-                url: 'http://jacinto.design',
+                name: 'AD Solutions',
+                url: 'https://alfreddominguez.com',
             },
         ];
         localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
